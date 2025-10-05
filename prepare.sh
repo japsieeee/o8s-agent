073d1d642396c1ce5eef60862733dcf2e03828c0
japs <ttest25@ip-172-31-5-131.ap-northeast-1.compute.internal>
2025-10-05 18:01:03 +0900

    chore: update script

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+PACKAGE_NAME=o8s-agent_1.0.0_amd64.deb
+
 npm run build &&
 
-nfpm package --packager deb --config nfpm.yaml --target dist/
+nfpm package --packager deb --config nfpm.yaml --target dist/ &&
+
+
+rm -rf /tmp/$PACKAGE_NAME &&
+
+mv ./dist/$PACKAGE_NAME /tmp/$PACKAGE_NAME &&
+
+sudo apt install /tmp/$PACKAGE_NAME
